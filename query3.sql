355812ee4ee12e7d3743323a215fa159679ccdcd
ariekas <ariariekas@gmail.com>
2025-10-29 22:46:01 +0700

    feat: creating query sql task 3

--- query3.sql
+++ query3.sql
@@ -8,3 +8,11 @@ FROM actors as a
 JOIN roles as r ON a.id = r.actor_id
 GROUP BY a.first_name
 HAVING count(r.role)>5;
+
+SELECT d.first_name, d.last_name, count(m.name) as total_movies
+FROM movies_directors AS md
+JOIN directors as d ON md.director_id = d.id
+JOIN movies as m ON md.movie_id = m.id
+GROUP BY d.first_name, d.last_name
+ORDER BY count(m.name) DESC
+LIMIT 1;
