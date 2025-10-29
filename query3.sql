d260ba652a372ada76488f8ce323436d982bfc0f
ariekas <ariariekas@gmail.com>
2025-10-29 22:45:36 +0700

    feat: update sql code taks 1 and create query sql code taks 2

--- query3.sql
+++ query3.sql
@@ -1,5 +1,10 @@
 SELECT id, first_name, last_name, count(genre)AS total_gendre_direct FROM directors
 JOIN directors_genres ON director_id = directors.id
-GROUP BY id, director_id;
+GROUP BY id;
 
 
+SELECT a.first_name, couqqnt(r.role) as total_role
+FROM actors as a
+JOIN roles as r ON a.id = r.actor_id
+GROUP BY a.first_name
+HAVING count(r.role)>5;
