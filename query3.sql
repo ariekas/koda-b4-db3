b58df92fb591fa859fc29a8a49e3cbf28636bc92
ariekas <ariariekas@gmail.com>
2025-10-30 09:12:01 +0700

    fix: fixxing typo count

--- query3.sql
+++ query3.sql
@@ -3,7 +3,7 @@ JOIN directors_genres ON director_id = directors.id
 GROUP BY id;
 
 
-SELECT a.first_name, couqqnt(r.role) as total_role
+SELECT a.first_name, count(r.role) as total_role
 FROM actors as a
 JOIN roles as r ON a.id = r.actor_id
 GROUP BY a.first_name
